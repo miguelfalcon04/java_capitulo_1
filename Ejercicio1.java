1b5c8044f006ed1195e1f1bff585af29f6ec51f6
miguelfal <falconmiguel.2004@gmail.com>
2022-10-06 10:12:29 +0200

    ejercicio1.java

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,12 +1,5 @@
-import java.util.Scanner;
 public class Ejercicio1 { // Clase principal
 public static void main(String[] args) {
-Scanner sc = new Scanner (System.in);
-System.out.print("Introduza su nombre: ");
-int nombre = sc.nextInt();
-System.out.print("Introduzca su apellido: ");
-int apellido =sc.nextInt();
-System.out.print( nombre + apellido);
-sc.close();
+System.out.print("Miguel Jesús Falcón Carvajal ");
 }
 }

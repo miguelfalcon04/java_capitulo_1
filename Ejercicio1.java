ad10b1136674443a67e4c55e4a0e565e7f7ef323
miguelfal <falconmiguel.2004@gmail.com>
2022-10-06 09:14:22 +0200

    added ejercicio1.java

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,9 +1,5 @@
-**
-* Muestra por pantalla la frase "¡Hola mundo!"
-*
-* @author miguelfalcon
-*/
-public class HolaMundo { // Clase principal
+
+public class Ejercicio1 { // Clase principal
 public static void main(String[] args) {
 System.out.println("¡Hola mundo!");
 }

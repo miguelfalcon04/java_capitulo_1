e52ba5e7a49e85832bc939d4ec8562d01460275e
miguelfal <falconmiguel.2004@gmail.com>
2022-10-06 09:14:04 +0200

    added ejercicio1.java

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -1,9 +1,5 @@
-**
-* Muestra por pantalla la frase "Adios mundo!"
-*
-* @author miguelfalcon
-*/
-public class HolaMundo { // Clase principal
+
+public class Ejercicio2 { // Clase principal
 public static void main(String[] args) {
 System.out.println("¡Adios mundo!");
 }

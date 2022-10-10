715fb118eeccf51c6c66dfa7e9d8cbfe53890f1b
miguelfal <falconmiguel.2004@gmail.com>
2022-10-10 21:18:18 +0200

    commit

---
+++ Ejercicio8.java
@@ -0,0 +1,11 @@
+public class Ejercicio8 {
+    public static void main(String[] args) {
+        System.out.println("\033[34m"); 
+        System.out.println("*********");
+        System.out.println(" *     *");
+        System.out.println("  *   *");
+        System.out.println("   * *");
+        System.out.println("    *");
+        System.out.println("\033[37m"); 
+      }
+}
